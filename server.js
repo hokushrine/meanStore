7816f5ab6a9d5c46b90b9325f84cd3e073b52dad
shma <shmaschool@gmail.com>
2016-02-13 18:41:30 -0800

    Mean Store
    Online Store built with MEAN Stack
    
    - currently has templating error

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname+'/client')));
+
+
+require('./server/config/mongoose.js');
+require('./server/config/routes.js')(app);
+
+app.listen(8000,function(){
+	console.log('Listening on port 8000.');
+});
+
+// start mongoose server first before
+// starting server.js
